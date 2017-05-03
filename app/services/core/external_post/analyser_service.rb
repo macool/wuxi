ef4eb4b1c1973701a39e3bd855f3ab889a131166
Mario Andrés Correa <a@macool.me>
2017-05-03 17:32:50 -0500

    allow reposting whitelisted users

--- app/services/core/external_post/analyser_service.rb
+++ app/services/core/external_post/analyser_service.rb
@@ -6,17 +6,22 @@ module Core
       end
 
       def analyse!
-        sentiment_analysis = SentimentAnalyserService.new(
-          external_post: @external_post
-        ).analyse!
         @external_post.update!(status: :analysed)
-
         whitelist = @external_post.external_user.status.whitelist?
         repost_all = @external_post.external_provider.repost.all?
-        if sentiment_analysis.positive? && (whitelist || repost_all)
+        if whitelist || repost_all || sentiment_analysis.positive?
           # reposting handled by the speaker
           @external_post.update!(status: :will_repost)
+          sentiment_analysis # trigger analysis just in case
         end
+      end
+
+      private
+
+      def sentiment_analysis
+        @sentiment_analysis ||= SentimentAnalyserService.new(
+          external_post: @external_post
+        ).analyse!
       end
     end
   end
